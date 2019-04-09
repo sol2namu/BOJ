ab2c7984fd792a1b2982d4a7feeb1ad12bfd6fd7
sol2namu <45122059+sol2namu@users.noreply.github.com>
2019-04-09 20:35:18 +0900

    sort

---
+++ countingsort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int arr[100];
+
+void countingSort(){
+	const int k = 1000;
+	int count[k + 1];
+	for (int i = 0; i < k + 1; i++){
+		count[i] = 0;
+	}
+
+	for (int i = 0; i < n; i++){
+		count[arr[i]]++;
+	}
+
+	for (int i = 1; i < k + 1; i++){
+		count[i] += count[i - 1];
+	}
+
+	int sort[k + 1], index = 0;
+
+	for (int i = n - 1; i >= 0; i--){
+		sort[count[arr[i]] - 1] = arr[i];
+		count[arr[i]]--;
+	}
+
+	for (int i = 0; i < n; i++) arr[i] = sort[i];
+}
+
+
+
+int main(){
+	cin >> n;
+  
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	countingSort();
+}
